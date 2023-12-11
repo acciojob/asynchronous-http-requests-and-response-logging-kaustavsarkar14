53e2a9cb31ab306688e9f9d3385f2b93dc72c3f3
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-12-11 18:26:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const urls = [
+  'https://jsonplaceholder.typicode.com/posts/1',
+  'https://jsonplaceholder.typicode.com/posts/2',
+  'https://jsonplaceholder.typicode.com/posts/3'
+];
+urls.forEach(url=>{
+	fetch(url)
+	.then(data=>data.json())
+	.then(data=>console.log(data))
+})
